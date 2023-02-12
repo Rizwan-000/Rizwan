01accdafac1ec408a0a0bcc6c9c2bdf5a230c606
Rizwan-000 <124767506+Rizwan-000@users.noreply.github.com>
2023-02-12 21:49:00 +0500

    Add files via upload

---
+++ My work.py
@@ -0,0 +1,55 @@
+print("hello")
+#sting
+y="Rizwan"
+print(y)
+
+
+#variable
+x= 15
+y= "Rizwan"
+print(x,y)
+
+#list
+list=["Rizwan", "Danish", "786"]
+print(list)
+
+#tuple 
+tuple = ("Rizwan", "Danish", "786")
+print(tuple)
+
+#set
+set= {"Rizwan", "Danish", "786"}
+print(set)
+
+#dictionaries
+dict = {
+    "name": "Rizwan",
+        "roll no": 33
+        }
+print(dict)
+
+#if-else code 
+a= 300
+b= 45
+if b > a:
+    print("b is greater than a")
+else:
+    print("b is not greater than a")   
+    
+#for loop
+City= ["Mailsi", "Bahawalpur", "lahore"]
+for x in City:
+    print(x)
+    
+#while loop
+i= 1
+while i < 7:
+    print(i)
+    if i == 5:
+        break
+    i+= 5
+    
+#function 
+def my_function():
+    print("i am a human")     
+my_function()
